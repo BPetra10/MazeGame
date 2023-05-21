48aad78e89c04cddf1e41576bb5d5cfa0759ec36
Bodnár Petra <bodnar.petra9@gmail.com>
2023-05-21 14:38:59 +0200

    Adding fieldMaker and populateGrid method, checking gameBoard settings.

--- src/main/java/maze/javafx/controller/GameController.java
+++ src/main/java/maze/javafx/controller/GameController.java
@@ -3,12 +3,18 @@ package maze.javafx.controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import maze.state.Table;
 import org.tinylog.Logger;
 
 public class GameController {
     @FXML
     private Label messageLabel;
     private String playerName;
+    @FXML
+    private GridPane gameBoard;
+    private Table table = new Table();
+    private StackPane[][] table_fields = new StackPane[7][7];
 
     /**
      * Setting playerName variable value and "adding to" messageLabel.
@@ -24,7 +30,34 @@ public class GameController {
     @FXML
     public void initialize()
     {
+        populateGrid();
+
         Platform.runLater(() ->messageLabel.setText(String.format("Good luck, %s!", playerName)));
         Logger.info("Starting game");
+    }
+
+    private void populateGrid()
+    {
+        for (int row = 0; row < 7; row++) {
+            for (int col = 0; col < 7; col++) {
+                table_fields[row][col] = fieldMaker(row,col);
+                gameBoard.add(table_fields[row][col],col,row);
+            }
+        }
+    }
+
+    private StackPane fieldMaker(int i, int j)
+    {
+        var field = new StackPane();
+
+        double top = table.getFields()[i][j].isTop() ? 5 : 1;
+        double bottom = table.getFields()[i][j].isBottom() ? 5 : 1;
+        double left = table.getFields()[i][j].isLeft() ? 5 : 1;
+        double right = table.getFields()[i][j].isRight() ? 5 : 1;
+
+        field.setBorder(new Border(new BorderStroke(null,null,null,null,
+                BorderStrokeStyle.SOLID,BorderStrokeStyle.SOLID,BorderStrokeStyle.SOLID,BorderStrokeStyle.SOLID,
+                null, new BorderWidths(top,right,bottom,left),null)));
+        return field;
     }
 }
