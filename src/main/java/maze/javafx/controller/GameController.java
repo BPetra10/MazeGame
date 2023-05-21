df326b3255e4e3d97540d96857668ebc00600126
Bodnár Petra <bodnar.petra9@gmail.com>
2023-05-21 13:42:42 +0200

    Check player name, and setting it. Handling start button.

--- src/main/java/maze/javafx/controller/GameController.java
+++ src/main/java/maze/javafx/controller/GameController.java
@@ -1,4 +1,30 @@
 package maze.javafx.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.tinylog.Logger;
+
 public class GameController {
+    @FXML
+    private Label messageLabel;
+    private String playerName;
+
+    /**
+     * Setting playerName variable value and "adding to" messageLabel.
+     * @param playerName is the actual player name.
+     * */
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    /**
+     * This method will initialize the game and set the default state.
+     * */
+    @FXML
+    public void initialize()
+    {
+        Platform.runLater(() ->messageLabel.setText(String.format("Good luck, %s!", playerName)));
+        Logger.info("Starting game");
+    }
 }
--- src/main/java/maze/javafx/controller/OpeningController.java
+++ src/main/java/maze/javafx/controller/OpeningController.java
@@ -1,4 +1,43 @@
 package maze.javafx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+import utility.javafx.ControllerHelper;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
 public class OpeningController {
+    @Inject
+    private FXMLLoader fxmlLoader = new FXMLLoader();
+    @FXML
+    private TextField playerNameTextField;
+    @FXML
+    private Label errorLabel;
+
+    /**
+     * This will load the game.fxml, if the player gives a name.
+     * @param actionEvent handling Start Button action.
+     * @throws IOException this will happen, if there is some I/O error while loading FXML.
+     * */
+    public void handleStartButton(ActionEvent actionEvent) throws IOException {
+        if (playerNameTextField.getText().isEmpty())
+        {
+            errorLabel.setText("Please enter your name!");
+            Logger.warn("Name field is empty.");
+        }
+        else
+        {
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            ControllerHelper.loadAndShowFXML(fxmlLoader,"/fxml/game.fxml",stage);
+            fxmlLoader.<GameController>getController().setPlayerName(playerNameTextField.getText());
+            Logger.info("The user's name is set to {}, loading game scene.", playerNameTextField.getText());
+        }
+    }
 }
