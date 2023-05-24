76afac18fdd413bc947417abcf557f7e6dd32c2d
Bodnár Petra <bodnar.petra9@gmail.com>
2023-05-24 09:13:37 +0200

    Handling Restart in HighScoresController

--- src/main/java/maze/javafx/controller/HighScoresController.java
+++ src/main/java/maze/javafx/controller/HighScoresController.java
@@ -2,15 +2,23 @@ package maze.javafx.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import maze.results.GameResult;
 import maze.results.HighScore;
 import maze.results.ScoreReadWrite;
 import org.tinylog.Logger;
+import utility.javafx.ControllerHelper;
 
+import javax.inject.Inject;
+import java.io.IOException;
 import java.time.Duration;
 
 /**
@@ -29,6 +37,9 @@ public class HighScoresController {
     @FXML
     private TableView<GameResult> highScoreTable;
 
+    @Inject
+    private FXMLLoader fxmlLoader = new FXMLLoader();
+
     @FXML
     private void initialize() {
         Logger.debug("Loading high scores...");
@@ -44,4 +55,16 @@ public class HighScoresController {
         observableResult.addAll(hs.getHighScores());
         highScoreTable.setItems(observableResult);
     }
+
+    /**
+     * Going back to opening.fxml.
+     * @param actionEvent handling Restart button.
+     * @throws IOException this will happen, if there is some I/O error while loading FXML.
+     * */
+    public void handleRestartButton(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Logger.debug("{} is pressed", ((Button) actionEvent.getSource()).getText());
+        ControllerHelper.loadAndShowFXML(fxmlLoader, "/fxml/opening.fxml", stage);
+    }
+
 }
